c583ab055ba08b44ea96de92c3741097b3de1a15
iamshiv007 <iamshiv20032003@gmail.com>
2023-10-09 17:57:53 +0530

    update: Portfolio TechStack  update

--- src/components/portfolio/pages/techStack/TechStack.jsx
+++ src/components/portfolio/pages/techStack/TechStack.jsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { Fragment, useState, useEffect, useRef } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 import { FaLaptopCode } from "react-icons/fa";
 
 import { TechStackOptions } from "@/components/application/layout/input/optionsData/techStackOptions";
@@ -8,41 +9,8 @@ import { TechStackOptions } from "@/components/application/layout/input/optionsD
 const TechStack = () => {
   const [section, setSection] = useState("Advance");
   const [sectionData, setSectionData] = useState([]);
-  const [istechStack, setIsTechStack] = useState(false);
-  const techStackRef = useRef();
-  const techBoxesRef = useRef();
-  const buttonsRef = useRef();
 
   const { techStack, loading } = useSelector((state) => state.techStackData);
-
-  // Scroll Animation
-  useEffect(() => {
-    if (techStackRef.current) {
-      const getScreenWidth = () =>
-        window.innerWidth ||
-        document.documentElement.clientWidth ||
-        document.body.clientWidth;
-
-      const techStackObserver = new IntersectionObserver(
-        ([techStackEntry]) => {
-          setIsTechStack(techStackEntry.isIntersecting);
-        },
-        {
-          rootMargin: `${getScreenWidth() <= 700 ? "-100px" : "-250px"}`,
-        }
-      );
-
-      techStackObserver.observe(techStackRef.current);
-
-      if (istechStack) {
-        techBoxesRef.current.classList.add("pop-up-child");
-        buttonsRef.current.classList.add("pop-up");
-      } else {
-        techBoxesRef.current.classList.remove("pop-up-child");
-        buttonsRef.current.classList.remove("pop-up");
-      }
-    }
-  }, [istechStack, techStack, techStackRef]);
 
   // Tech Stack Section Change
   useEffect(() => {
@@ -69,9 +37,6 @@ const TechStack = () => {
         );
       }
     }
-    setTimeout(() => {
-      techBoxesRef?.current?.classList.add("pop-up-child");
-    }, 300);
   }, [techStack, section]);
 
   const sections = ["Advance", "Good", "Familiar"];
@@ -79,57 +44,54 @@ const TechStack = () => {
   return (
     <Fragment>
       {!loading && techStack?.userName && (
-        <section
-          className='shadow-zinc-300 dark:shadow-zinc-700 shadow-sm overflow-hidden'
-          id='techStack'
-          ref={techStackRef}
-        >
-          <h2 className='text-3xl font-bold text-center pt-4 pb-8 flex justify-center items-center gap-3'>
+        <section className={style.mainContainer} id='techStack'>
+          <h2 className='pageHeading'>
             <FaLaptopCode /> Tech Stack
           </h2>
-
-          <div
-            className='pop-down transition-all w-fit duration-500 m-auto rounded-lg border border-black dark:border-white border-solid overflow-hidden'
-            ref={buttonsRef}
+          {/* Button Group */}
+          <motion.div
+            className={style.btnGroup}
+            initial={{ opacity: 0, scale: 0 }}
+            transition={{ duration: 0.5 }}
+            viewport={{ once: true }}
+            whileInView={{ opacity: 1, scale: 1 }}
           >
             {/* Section Buttons Group */}
             {sections.map((sectionName) => (
               <button
-                className={`text-sm md:text-base text-black dark:text-white w-[100px] md:w-[150px] p-2 font-bold hover:bg-gray-200 dark:hover:bg-gray-700 ${
+                className={`${style.btn} ${
                   section === sectionName ? "bg-red-600" : null
-                } transition-all`}
+                }`}
                 key={sectionName}
                 onClick={(e) => {
                   setSection(sectionName);
-                  if (section !== sectionName)
-                    techBoxesRef.current.classList.remove("pop-up-child");
                 }}
               >
                 {sectionName}
               </button>
             ))}
-          </div>
+          </motion.div>
           {/* Tech Boxes */}
-          <div
-            className='pop-down-child flex min-h-[400px] py-[30px] px-[20px] md:px-[100px] flex-wrap justify-center items-center gap-5'
-            ref={techBoxesRef}
-          >
+          <div className={style.boxesWrapper}>
             {sectionData.length !== 0 ? (
               sectionData.map((tech) => (
-                <div
-                  className='transition-all duration-700 px-2 h-fit py-3 md:py-5 w-[80px] md:w-[150px] border border-black dark:border-white border-solid rounded flex flex-col gap-3 items-center'
+                // Tech Box
+                <motion.div
+                  className={style.box}
+                  initial={{ opacity: 0, scale: 0 }}
                   key={tech.name}
+                  transition={{ duration: 0.5 }}
+                  viewport={{ once: true }}
+                  whileInView={{ opacity: 1, scale: 1 }}
                 >
                   {/* Tech Icon */}
                   <p>{tech.icon}</p>
                   {/* Tech Name */}
-                  <p className='text-sm md:text-base text-center'>
-                    {tech.name}
-                  </p>
-                </div>
+                  <p className={style.techName}>{tech.name}</p>
+                </motion.div>
               ))
             ) : (
-              <p className='transition-all duration-700 text-3xl'>No Skills</p>
+              <p className={style.noSkillText}>No Skills</p>
             )}
           </div>
         </section>
@@ -139,3 +101,16 @@ const TechStack = () => {
 };
 
 export default TechStack;
+
+const style = {
+  mainContainer:
+    "shadow-zinc-300 dark:shadow-zinc-700 shadow-sm overflow-hidden",
+  btnGroup:
+    "transition-all w-fit duration-500 m-auto rounded-lg border border-black dark:border-white border-solid overflow-hidden",
+  btn: "text-sm md:text-base text-black dark:text-white w-[100px] md:w-[150px] p-2 font-bold hover:bg-gray-200 dark:hover:bg-gray-700 transition-all",
+  boxesWrapper:
+    "flex min-h-[400px] py-[30px] px-[20px] md:px-[100px] flex-wrap justify-center items-center gap-5",
+  box: "px-2 h-fit py-3 md:py-5 w-[80px] md:w-[150px] border border-black dark:border-white border-solid rounded flex flex-col gap-3 items-center",
+  techName: "text-sm md:text-base text-center",
+  noSkillText: "transition-all duration-700 text-3xl",
+};
